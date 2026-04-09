8635b36521365a1ab569330edaf7dedbca784611
nasmaaa-cs <nasmaaa@aucegypt.edu>
2026-04-09 21:57:57 +0200

    Updated Player.h file

--- include/Player.h
+++ include/Player.h
@@ -1,1 +1,32 @@
 // Player.h - Player class definition
+#ifndef PLAYER_H
+#define PLAYER_H
+
+#include "Character.h"
+#include "InputHandler.h"
+
+enum PlayerType {
+	TANK,
+	FAST,
+	BALANCED
+};
+
+class Player : public Character {
+private:
+	int score;
+	PlayerType type;
+	InputHandler* input;
+
+	void setStats();
+
+public:
+	Player(PlayerType t, InputHandler* input);
+
+	void move() override;
+	void attack(Character* target) override;
+
+	void addScore(int s);
+	int getScore() const;
+};
+
+#endif
