9bf2419f2942c2ee127f49d9bac022ab9dffc6c7
yousef-elaraby <yousefelaraby@aucegypt.edu>
2026-04-11 01:01:05 +0200

    Character cpp class
    yousef elaraby character cpp class

--- src/Character.cpp
+++ src/Character.cpp
@@ -1,1 +1,17 @@
-// Character.cpp - Character implementation
+#include "Character.h"
+
+Character::Character(int hp, int maxHp, float x, float y, float speed, int attackPower)
+    : hp(hp), maxHp(maxHp), x(x), y(y), speed(speed), attackPower(attackPower) {}
+
+Character::~Character() {}
+
+void Character::takeDamage(int dmg) {
+    hp -= dmg;
+    if (hp < 0) {
+        hp = 0;
+    }
+}
+
+bool Character::isAlive() const {
+    return hp > 0;
+}
